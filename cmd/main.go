422c18d3134543dc8979245741e4a411d7344b9c
pgombola <pgombola@cleo.com>
2017-05-10 16:21:06 -0400

    Added discovery of service installer jar

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -53,12 +55,18 @@ func main() {
 	}
 	peers, _ := config.peers()
 
+	args, err := newArgs(config.Clarify, node, peers)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	cmd := &exec.Cmd{
 		Dir:  config.Clarify.Install,
 		Path: "jre/bin/java",
-		Args: newArgs(config.Clarify, node, peers).Args,
+		Args: args.Args,
 	}
 
+	fmt.Printf("Command: %s %v\n", cmd.Dir+cmd.Path, strings.Join(cmd.Args, " "))
 	if err := cmd.Run(); err != nil {
 		fmt.Println(err)
 	}
@@ -99,8 +107,13 @@ func (n *config) peers() (string, error) {
 	return strings.Join(peers, " "), nil
 }
 
-func newArgs(c *clarify, n *node, peers string) *args {
+func newArgs(c *clarify, n *node, peers string) (*args, error) {
 	args := &args{}
+	jar, err := findInstallerJar(c.Install)
+	if err != nil {
+		return args, err
+	}
+	args.jar(jar)
 	args.user(c.User)
 	args.toolsInstall(n.Tools)
 	args.clarifyInstall(c.Install)
@@ -109,7 +122,28 @@ func newArgs(c *clarify, n *node, peers string) *args {
 	args.address(n.Address)
 	args.nomad(c.NomadPort)
 	args.hosts(peers)
-	return args
+	return args, nil
+}
+
+func findInstallerJar(install string) (string, error) {
+	var jar string
+	err := filepath.Walk(path.Join(install, "tools", "lib"),
+		func(filepath string, info os.FileInfo, err error) error {
+			matched, err := path.Match("clarify-server-installer-*", info.Name())
+			if err != nil {
+				return err
+			}
+			if matched {
+				jar = filepath
+			}
+			return nil
+		})
+	if err != nil {
+		return "", err
+	} else if len(jar) == 0 {
+		return "", errors.New("unable to locate service installer jar")
+	}
+	return jar, nil
 }
 
 func (a *args) user(user string) {
@@ -154,4 +188,9 @@ func (a *args) nomad(port int) {
 func (a *args) hosts(peers string) {
 	a.Args = append(a.Args, "-hosts")
 	a.Args = append(a.Args, peers)
+}
+
+func (a *args) jar(jar string) {
+	a.Args = append(a.Args, "-jar")
+	a.Args = append(a.Args, jar)
 }
